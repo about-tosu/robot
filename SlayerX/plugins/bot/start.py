92f97541f15938ff71d0edc9ac77b330260115f0
tusarpatel <167279662+tusarpatel@users.noreply.github.com>
2024-05-23 19:52:27 +0530

    Update start.py

--- SlayerX/plugins/bot/start.py
+++ SlayerX/plugins/bot/start.py
@@ -46,7 +46,7 @@ async def start_pm(client, message: Message, _):
                 )
             return
         if name[0:3] == "inf":
-            m = await message.reply_text("❤‍🩹")
+            m = await message.reply_text("🍾")
             query = (str(name)).replace("info_", "", 1)
             query = f"https://www.youtube.com/watch?v={query}"
             results = VideosSearch(query, limit=1)
